9710f67b5a3ef84c221323010d76019452e0783a
SNUPI <47105088+joohaem@users.noreply.github.com>
2021-07-24 18:20:27 +0900

    add UI css - joam

---
+++ emotional_chatbot_Interface-feature-onlyUI/README.md
@@ -0,0 +1,77 @@
+# EMOTIONAL CHATBOT Interface
+
+
+This project aims to develop meorable and emotional chatbot using transfer learning (fine tune GPT-2 345M). You can find original code [here](https://github.com/openai/gpt-2).
+
+This is gpt-2 chatbot using interface.
+
+It is never designed for commercial purposes.
+
+
+![ex_screenshot](./static/img/botAvatar.png)
+
+
+## Install python library
+```
+$ pip install -r requirements.txt
+```
+
+## Model install
+1) clik the [link](https://drive.google.com/file/d/1CzCNAuaXiaQsdCMTiki2X9XuyCwowQY3/view?usp=sharing) and download.
+2) Place the downloaded model in ***models\345M_org.***.
+
+
+
+
+## Usage
+
+just run **_main.py_**.
+
+```
+$ python main.py
+```
+
+and go to "http://localhost:5000".
+
+you can see this page.
+
+![ex_screenshot](./img/2021-04-12%2020-05-54.png)
+
+you can use **_text mode_** and **_speech mode_**.
+
+### text mode
+click toggle and type your message.
+
+![ex_screenshot](./img/2021-04-12%2020-08-48.png)
+
+### speech mode
+
+if you want to use speech mode, you need google **_speech-to-text key_**. refer this page [*speech to text*](https://www.youtube.com/watch?v=Ds-7D8d-FwA&t=1154s)
+
+if you get **_key.json_**, replace key.json with your **_key.json_**
+
+
+then click toggle and click **_ChatBot_** .  text color changes to black. now you can to the chatbot with your mic.
+
+![ex_screenshot](./img/2021-04-12%2020-14-57.png)
+
+## Author
+Jungseob Lee / [ js-lee-AI](https://github.com/js-lee-AI) / omanma1928@naver.com
+
+Jungmu Park / [boong_u](https://github.com/qkrwjdan) / madogisa12@naver.com
+
+## Related papers
+A Radford, et al., ["Language Models are Unsupervised Multitask Learners"](https://d4mucfpksywv.cloudfront.net/better-language-models/language-models.pdf), openAI blog, 2019.
+
+A Vaswani, et al., ["Attention is All you Need"](https://arxiv.org/pdf/1706.03762.pdf), NIPS 2017
+
+## References
+[*openAI*](https://github.com/openai/gpt-2)
+
+[*emotional chatbot*](https://github.com/js-lee-AI/emotional-chatbot_gpt2)
+
+[*speech to text*](https://www.youtube.com/watch?v=Ds-7D8d-FwA&t=1154s)
+
+[*rasa chatbot interface youtube*](https://www.youtube.com/watch?v=Fyap_IP1i3Q&t=98s)
+
+[*interface*](https://github.com/GKTechy/rasa_core_chatbot)
---
+++ emotional_chatbot_Interface-feature-onlyUI/static/js/chat.js
@@ -0,0 +1,103 @@
+// on input/text enter--------------------------------------------------------------------------------------
+$('.usrInput').on('keyup keypress', function (e) {
+    var keyCode = e.keyCode || e.which;
+    var text = $(".usrInput").val();
+    if (keyCode === 13) {
+        if (text == "" || $.trim(text) == '') {
+            e.preventDefault();
+            return false;
+        } else {
+            $(".usrInput").blur();
+            setUserResponse(text);
+            send(text);
+            e.preventDefault();
+            return false;
+        }
+    }
+});
+
+$('.send').on('click', function (e) {
+    var text = $(".usrInput").val();
+
+    if (text) {
+        setUserResponse(text);
+        send(text);
+        e.preventDefault();
+        return false;
+    }
+})
+
+//------------------------------------- Set user response------------------------------------
+function setUserResponse(val) {
+
+    // var userAvatarSrc = "{{ url_for('static',filename='/img/userAvatar.jpg') }}"
+    // var userAvatarSrc = "/static/img/userAvatar.jpg";
+    var userAvatarSrc = "/static/img/user.png";
+    var UserResponse = '<div class = "messageBox"><img class="userAvatar" src="'
+        + userAvatarSrc + '"><div class="userMsg">' + val + ' </div><div class="clearfix"></div></div>';
+    $(UserResponse).appendTo('.chats').show('slow');
+    $(".usrInput").val('');
+    scrollToBottomOfResults();
+    $('.suggestions').remove();
+}
+
+//---------------------------------- Scroll to the bottom of the chats-------------------------------
+function scrollToBottomOfResults() {
+    var terminalResultsDiv = document.getElementById('chats');
+    terminalResultsDiv.scrollTop = terminalResultsDiv.scrollHeight;
+}
+
+function send(message) {
+    console.log("User Message:", message)
+    $('#audio').attr("hidden", true)
+    $.ajax({
+        url: 'http://localhost:5000/chat/',
+        type: 'POST',
+        contentType: 'application/json',
+        data: JSON.stringify({
+            "message": message,
+            "sender": "Me"
+        }),
+        success: function (data, textStatus) {
+            console.log(data)
+            if (data != null) {
+                console.log("set bot response")
+                setBotResponse(data);
+                // synthesize(data.text);
+            }
+            console.log("Rasa Response: ", data, "\n Status:", textStatus)
+        },
+        error: function (errorMessage) {
+            console.log('Error' + errorMessage);
+
+        }
+    });
+}
+
+//------------------------------------ Set bot response -------------------------------------
+function setBotResponse(val) {
+    setTimeout(function () {
+
+        if (val.hasOwnProperty("text")) {
+            var BotAvatarSrc = "/static/img/botAvatar.png";
+            var BotResponse = '<div class="messageBox"><img class="botAvatar" src="'
+                + BotAvatarSrc + '"><div class="botMsg">' + val.text + '</div><div class="clearfix"></div></div>';
+            $(BotResponse).appendTo('.chats').hide().fadeIn(1000);
+        }
+
+        scrollToBottomOfResults();
+
+    }, 500);
+}
+
+// ------------------------------------------ Toggle chatbot -----------------------------------------------
+$('#profile_div').click(function () {
+    $('.profile_div').toggle();
+    $('.widget').toggle();
+    scrollToBottomOfResults();
+});
+
+$('#close').click(function () {
+    $('.profile_div').toggle();
+    $('.widget').toggle();
+});
