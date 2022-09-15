eb97825734c7148891be11da0f7406b73b45a800
qkrwjdan <madogisa12@naver.com>
2022-09-15 20:53:49 +0900

    Feat: eng chatbot 대답 가져오는 함수 작성

--- main.py
+++ main.py
@@ -6,6 +6,25 @@ import os
 import time
 
 app = Flask(__name__)
+ENG_CHATBOT_URL = os.environ.get("ENG_CHATBOT_URL")
+
+
+def send_message_to_eng_chatbot(input_text):
+    print(ENG_CHATBOT_URL)
+    print(os.environ)
+    headers = {'Content-Type': 'application/json; charset=utf-8'}
+    contents = {
+        'message': input_text,
+        'sender': "test"
+    }
+
+    gpt2_res = requests.post(
+        ENG_CHATBOT_URL, headers=headers, data=json.dumps(contents))
+    gpt2_data = gpt2_res.json()
+
+    gpt2_text = gpt2_data['message']
+
+    return gpt2_text
 
 
 @app.route('/english', methods=('GET', 'POST'))
@@ -13,7 +32,8 @@ def english():
     if request.method == 'POST':
         input_text = request.json['message']
         print(input_text)
-        answer = "testtesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttest"
+        answer = send_message_to_eng_chatbot(input_text)
+        print('answer : ', answer)
         context = {'text': answer, 'recipient_id': request.json['sender']}
         return context
 
