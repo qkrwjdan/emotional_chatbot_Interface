2e4758d073eb1f7874a0cd1addfc2c7de42a7b7e
kim-limit <rlawogks2468@naver.com>
2022-12-20 13:54:25 +0900

    feat: 'create helper modal'

---
+++ static/js/helperModal.js
@@ -0,0 +1,30 @@
+const modal = document.getElementById("modal");
+function modalOn() {
+  modal.style.display = "flex";
+}
+function isModalOn() {
+  return modal.style.display === "flex";
+}
+function modalOff() {
+  modal.style.display = "none";
+}
+const btnModal = document.getElementById("btn-modal");
+btnModal.addEventListener("click", (e) => {
+  modalOn();
+});
+const closeBtn = modal.querySelector(".close-area");
+closeBtn.addEventListener("click", (e) => {
+  console.log("클릭");
+  modalOff();
+});
+modal.addEventListener("click", (e) => {
+  const evTarget = e.target;
+  if (evTarget.classList.contains("modal-overlay")) {
+    modalOff();
+  }
+});
+window.addEventListener("keyup", (e) => {
+  if (isModalOn() && e.key === "Escape") {
+    modalOff();
+  }
+});
